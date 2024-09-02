82ae12be8a04633362bf06010166d022928fe3f9
csm <contatocsmpm@gmail.com>
2024-09-02 08:26:57 -0300

    fix-structure-pages-repositories

--- src/main/java/com/csmsolucoesedesenvolvimento/school/management/repositories/UserRepository.java
+++ src/main/java/com/csmsolucoesedesenvolvimento/school/management/repositories/UserRepository.java
@@ -1,4 +1,4 @@
-package com.csmsolucoesedesenvolvimento.school.repositories;
+package com.csmsolucoesedesenvolvimento.school.management.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
